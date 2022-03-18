8bee11b16706996d0eaba61bd10d977c32aee932
Romans-I-XVI <contact@romansixvigaming.com>
2022-03-18 11:17:55 -0500

    Added short and tall tree variation

--- HarpersPlatformerShared/Entities/Tree.cs
+++ HarpersPlatformerShared/Entities/Tree.cs
@@ -6,12 +6,19 @@ namespace HarpersPlatformer.Entities;
 
 public class Tree : LevelCanvasEntity
 {
-	public Tree(int x, int y) {
+
+	public Tree(Trees tree, int x, int y) {
 		this.Depth = BackgroundImage.DefaultDepth;
 		this.Position = new Vector2(x, y);
 
-		var tree_region = new Region(Engine.Game.Content.Load<Texture2D>("textures/tree_1"));
+		var tree_region = new Region(Engine.Game.Content.Load<Texture2D>("textures/" + tree));
 		tree_region.AutoOrigin(DrawFrom.BottomCenter);
 		this.AddSprite("main", new Sprite(tree_region));
 	}
+}
+
+public enum Trees
+{
+	tree_short,
+	tree_tall
 }
