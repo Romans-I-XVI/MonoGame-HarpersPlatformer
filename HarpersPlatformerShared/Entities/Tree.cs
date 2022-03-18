02644f48ab48ddd9622e796ef4a7b0955896010e
Romans-I-XVI <contact@romansixvigaming.com>
2022-03-18 13:04:32 -0500

    Added platforms to trees

--- HarpersPlatformerShared/Entities/Tree.cs
+++ HarpersPlatformerShared/Entities/Tree.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoEngine;
@@ -6,10 +7,20 @@ namespace HarpersPlatformer.Entities;
 
 public class Tree : LevelCanvasEntity
 {
+	private readonly int PlatformOffsetX = -85;
+	private readonly int PlatformWidth = 168;
+	private readonly int PlatformHeight = 50;
+	private readonly Dictionary<Trees, int> PlatformOffsetsY = new Dictionary<Trees, int> {
+		[Trees.tree_short] = -279,
+		[Trees.tree_tall] = -279 - 66
+	};
+	private readonly int PlatformOffsetY;
+	private readonly Platform Platform;
 
 	public Tree(Trees tree, int x, int y, float scale = 1f) {
 		this.Depth = BackgroundImage.DefaultDepth;
 		this.Position = new Vector2(x, y);
+		this.PlatformOffsetY = this.PlatformOffsetsY[tree];
 
 		var tree_region = new Region(Engine.Game.Content.Load<Texture2D>("textures/" + tree));
 		tree_region.AutoOrigin(DrawFrom.BottomCenter);
@@ -17,6 +28,27 @@ public class Tree : LevelCanvasEntity
 			Scale = new Vector2(scale)
 		};
 		this.AddSprite("main", tree_sprite);
+
+		var platform_rect = new Rectangle(
+			(int)this.Position.X + this.PlatformOffsetX,
+			(int)this.Position.Y + this.PlatformOffsetY,
+			this.PlatformWidth,
+			this.PlatformHeight
+		);
+		this.Platform = new Platform(platform_rect);
+		Engine.SpawnInstance(this.Platform);
+	}
+
+	public override void onUpdate(float deltaTime) {
+		base.onUpdate(deltaTime);
+
+		this.Platform.Position.X = this.Position.X + this.PlatformOffsetX;
+		this.Platform.Position.Y = this.Position.Y + this.PlatformOffsetY;
+	}
+
+	public override void onDestroy() {
+		base.onDestroy();
+		this.Platform.IsExpired = true;
 	}
 }
 
